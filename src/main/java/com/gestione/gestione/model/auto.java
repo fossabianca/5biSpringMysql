50beafb65bfdcffbf7ab4798779cb332a4a77812
fossabianca <francesco.bonomi03@gmail.com>
2022-05-24 15:48:12 +0200

    added setter and getter fro both model

--- src/main/java/com/gestione/gestione/model/auto.java
+++ src/main/java/com/gestione/gestione/model/auto.java
@@ -30,5 +30,43 @@ public class auto {
     @Column(name = "fkproprietario",nullable = false)
     private int fkproprietario;
 
+    public String getModello() {
+        return this.modello;
+    }
 
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+    
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+    public String getTarga() {
+        return this.targa;
+    }
+
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+     
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+      
+    public int getFkproprietario() {
+        return this.fkproprietario;
+    }
+
+    public void setFkproprietario(int fkproprietario) {
+        this.fkproprietario = fkproprietario;
+    }
 }
--- src/main/java/com/gestione/gestione/model/proprietario.java
+++ src/main/java/com/gestione/gestione/model/proprietario.java
@@ -26,5 +26,44 @@ public class proprietario {
     @Column(name = "telefono", nullable = false)
     private String telefono;
     
+    public long getId()
+    {
+        return id;        
+    }
+
+    public String getNome()
+    {
+        return nome;        
+    }
+
+    public String getCognome()
+    {
+        return cognome;        
+    }
+
+    public String getTelefono()
+    {
+        return telefono;        
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;        
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;        
+    }
+
+    public void setCognome(String cognome)
+    {
+        this.cognome = cognome;        
+    }
+
+    public void setTelefono(String telefono)
+    {
+        this.telefono = telefono;        
+    }
 
 }
